78aeead23713c9d8ae3f55535cb80f11deb79883
DozyHole <matthew.hoban@sky.com>
2019-07-17 18:50:31 +0100

    Updated project - new location

---
+++ StatFiddler_Project/Assets/Scripts/ScrText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScrText : MonoBehaviour {
+    public Transform Master;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Set()
+    {
+        if (Master)
+        {
+            GetComponent<InputField>().text = Master.GetComponent<Slider>().value.ToString();
+        }
+    }
+}
